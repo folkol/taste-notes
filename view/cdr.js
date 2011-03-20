30948b88786c9c6a378f27d39fd547a125275d98
fredriks <fredrik.stahl@atex.com>
2011-03-20 02:29:17 +0100

    printing 2! notes

--- view/cdr.js
+++ view/cdr.js
@@ -1,13 +1,25 @@
 
 getStuff = function() {
+    jQuery.getJSON("http://localhost:8000/notes/1",
+                  function(json) {
+                      jQuery("#note").append("<h3>Distilery: " + json.Distillery + "</h3>");
+                      jQuery("#note").append("<h3>Region: " + json.Region + "</h3>");
+                      jQuery("#note").append("<h3>Name: " + json.Name + "</h3>");
+                      jQuery("#note").append("<h3>Batch: " + json.Batch + "</h3>");
+                      jQuery("#note").append("<h3>Age: " + json.Age + "</h3>");
+                      jQuery("#note").append("<h3>Note: " + json.Note + "</h3>");
+                      jQuery("#note").append("<h3>Score: " + json.Score + "</h3>");
+                  });
+
     jQuery.getJSON("http://localhost:8000/notes/2",
                   function(json) {
-                      jQuery("#note").append("<h1>Distilery: " + json.Distillery + "</h1>");
-                      jQuery("#note").append("<h1>Region: " + json.Region + "</h1>");
-                      jQuery("#note").append("<h1>Name: " + json.Name + "</h1>");
-                      jQuery("#note").append("<h1>Batch: " + json.Batch + "</h1>");
-                      jQuery("#note").append("<h1>Age: " + json.Age + "</h1>");
-                      jQuery("#note").append("<h1>Note: " + json.Note + "</h1>");
-                      jQuery("#note").append("<h1>Score: " + json.Score + "</h1>");
+                      jQuery("#note").append("<hr />");
+                      jQuery("#note").append("<h3>Distilery: " + json.Distillery + "</h3>");
+                      jQuery("#note").append("<h3>Region: " + json.Region + "</h3>");
+                      jQuery("#note").append("<h3>Name: " + json.Name + "</h3>");
+                      jQuery("#note").append("<h3>Batch: " + json.Batch + "</h3>");
+                      jQuery("#note").append("<h3>Age: " + json.Age + "</h3>");
+                      jQuery("#note").append("<h3>Note: " + json.Note + "</h3>");
+                      jQuery("#note").append("<h3>Score: " + json.Score + "</h3>");
                   });
 };
